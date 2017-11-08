d1a16a5c5e69ff6ed3d77932f392012d0b5ca518
jonas <ferreirajonasss@gmail.com>
2017-11-07 23:33:10 -0200

    ajustes na database

--- dinapp/settings.py
+++ dinapp/settings.py
@@ -84,6 +84,11 @@ DATABASES = {
 }
 
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+#'HEROKU_POSTGRESQL_GOLD_URL'
+
+
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
