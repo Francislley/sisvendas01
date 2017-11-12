7038b1aa089df77a7fbdfb7e2e64cd4ac48a7b60
jonas <ferreirajonasss@gmail.com>
2017-11-12 12:06:48 -0200

    exibição do saldo pot usuario na tela inicial e ajustes no str dos models

--- venda/models.py
+++ venda/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 
 
 class Produto(models.Model):
-    
     nome = models.CharField(max_length=50)
 
     def __str__(self):
@@ -11,16 +10,19 @@ class Produto(models.Model):
 
 
 class Venda(models.Model):
-    
     usuario = models.ForeignKey(User)
     valor = models.FloatField()
     data_compra = models.DateField()
-    produto = models.ForeignKey(Produto)
+    produto = models.ForeignKey(Produto)    
+
+    def __str__(self):
+        return "R$:{0} ({1})".format(self.valor, self.usuario)
 
 
 class Pagamento(models.Model):
-
     valor = models.FloatField()
     usuario = models.ForeignKey(User)
     data_pagamento = models.DateField()
 
+    def __str__(self):
+        return "R$:{0} ({1})".format(self.valor, self.usuario)
--- venda/views.py
+++ venda/views.py
@@ -1,4 +1,6 @@
+from django.db.models import Sum
 from venda.models import Venda
+from venda.models import Pagamento
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
@@ -17,11 +19,27 @@ def login_user(request):
                 return redirect('/')
     return render(request, 'index.html', {})
 
+def calculate_total_pagamentos_to_user(request):
+    valor_total = Pagamento.objects.filter(
+        usuario=request.user).\
+        aggregate(valor_total=Sum('valor')).get('valor_total', 0.0)
+    return valor_total if valor_total else 0.0
+
+def calculate_total_vendas_to_user(request):
+    valor_total = Venda.objects.filter(
+        usuario=request.user).\
+        aggregate(valor_total=Sum('valor')).get('valor_total', 0.0)
+    return valor_total if valor_total else 0.0
+
 @login_required(login_url='/login/')
 def main(request):
     context = { 
-        'vendas':Venda.objects.filter(
-            usuario=request.user) 
+        'vendas': Venda.objects.filter(
+            usuario=request.user),
+        'pagamentos': Pagamento.objects.filter(
+            usuario=request.user),
+        'total_saldo': calculate_total_vendas_to_user(request) - \
+            calculate_total_pagamentos_to_user(request)
         }
     return render(request, 'main.html', context)
     
